60642d5f841870d4a2d3b4ca7b85ebb3e6863e41
Andrei Chenchik <andrei@chenchik.me>
2025-02-19 19:51:45 +0100

    Initial Commit

---
+++ Web3Chat/ContentView.swift
@@ -0,0 +1,26 @@
+// ContentView.swift is a part of Web3Chat project
+//
+// Created by Andrei Chenchik (andrei@chenchik.me), 19/2/25
+// Copyright © 2025 Andrei Chenchik, Inc. All rights reserved.
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// 
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Web3Chat/Web3ChatApp.swift
@@ -0,0 +1,19 @@
+// Web3ChatApp.swift is a part of Web3Chat project
+//
+// Created by Andrei Chenchik (andrei@chenchik.me), 19/2/25
+// Copyright © 2025 Andrei Chenchik, Inc. All rights reserved.
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// 
+
+
+import SwiftUI
+
+@main
+struct Web3ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
